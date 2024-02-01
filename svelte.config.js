47aadb27c975de55f67d323cede1a1db254387c7
Francois Duquesne <francois.duquesne@sealab.be>
2024-02-01 01:20:15 +0100

    changed build path

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,9 @@ const config = {
 		adapter: adapter({
 			fallback: undefined,
 		}),
-		// paths: {
-		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		// }
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
