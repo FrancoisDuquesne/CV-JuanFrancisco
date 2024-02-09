7ae6239fa05f0d0bb548ea71d3c5c166beb60901
Francois Duquesne <32546998+FrancoisDuquesne@users.noreply.github.com>
2024-02-09 02:18:26 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 		github: 'https://github.com/juan15-fran'
 	};
 
-	let languages = ['Spanish (native)', 'English (fluent)', 'French (fluent)'];
+	let languages = ['Spanish (native)', 'English (fluent)', 'French (fluent)', 'hola'  ];
 
 	let showExperience = true;
 	let showEducation = true;
